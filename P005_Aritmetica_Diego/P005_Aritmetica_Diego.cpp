bb58b0c306ccbec5a8ebfe9e1815515279907d0c
Scoupp666 <144696601+Scoupp666@users.noreply.github.com>
2023-09-27 08:56:41 -0600

    P005_Aritmetica
    subo los cambios de la actividad P005

--- P005_Aritmetica_Diego/P005_Aritmetica_Diego.cpp
+++ P005_Aritmetica_Diego/P005_Aritmetica_Diego.cpp
@@ -6,10 +6,10 @@
 #include <iostream>
 #include <math.h>
 
-    int main()
-    {
+int main()
+{
         //Declaración de variables
-        float a = 0;
+    float a = 0;
         float b = 0;
         float c = 0;
         float d = 0;
@@ -36,23 +36,29 @@
         std::cin >> a;
         std::cout << "ingresa el numero por el que lo quieres sumar: \n";
         std::cin >> b;
-        a + b = c;
+        c = a + b;
         std::cout << "El resultado de la suma es" << c << std::endl;
-        a - b = c;
-        std::cout << "El resultado ed la resta es" << c << std::endl;
-        a * b + c;
+
+        std::cout << "ingresa un numero: \n";
+        std::cin >> a;
+        std::cout << "ingresa el numero por el que lo quieres restar: \n";
+        std::cin >> b;
+        c = a - b;
+        std::cout << "El resultado de la resta es" << c << std::endl;
+
+        std::cout << "ingresa un numero: \n"; 
+        std::cin >> a;
+        std::cout << "ingresa el numero por el que lo quieres multiplicar: \n";
+        std::cin >> b;
+        c = a * b;
         std::cout << "El resultado de la multiplicacion es" << c << std::endl;
-        a / b = 0;
-        std::cout << "El resultado de la division es" << c << std::endl;
 
-    }
+        std::cout << "ingresa un numero: \n";
+        std::cin >> a;
+        std::cout << "ingresa el numero por el que lo quieres dividir: \n";
+        std::cin >> b;
+        c = a / b;
+        std::cout << "El resultado de la division es" << c << std::endl; 
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+}
