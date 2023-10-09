aafba2107649c5ac6cc14d08e7bc020719fd6b84
Scoupp666 <144696601+Scoupp666@users.noreply.github.com>
2023-10-08 20:04:23 -0600

    P010

---
+++ P011_Condicionales_V0/P011_Condicionales_V0.cpp
@@ -0,0 +1,37 @@
+// P011_Condicionales_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include <locale>
+
+int main() {
+    std::setlocale(LC_ALL, "es_MX.UTF-8");
+    std::cout << "Piensa en un Videojuego (Halo infinite, Star Wars BF2)." << std::endl;
+    std::cout << "Responda 1 para 'si' o 0 para 'no' a las preguntas." << std::endl;
+    std::string respuesta;
+    bool repetir = true;
+
+    while (repetir)
+    {
+     
+        bool = true;
+        1 
+        std::cout << "Deseas repetir el algoritmo? 0.- NO 1.- SI: ";
+        std::cin >> ;
+        system("cls");
+
+    }
+    return 0; 
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
