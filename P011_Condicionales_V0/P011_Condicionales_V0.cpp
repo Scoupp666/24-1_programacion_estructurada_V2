01dc4ef7fa0c1ee9f0e5a8adb4d2f05afb10d7f7
Scoupp666 <144696601+Scoupp666@users.noreply.github.com>
2023-10-10 00:16:55 -0600

    P011

--- P011_Condicionales_V0/P011_Condicionales_V0.cpp
+++ P011_Condicionales_V0/P011_Condicionales_V0.cpp
@@ -1,30 +1,124 @@
 // P011_Condicionales_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// Diego Delfin Garibi, zap994, 9/10/2023
+// Realizar preguntas para adivinar “tema libre” 
 
 #include <iostream>
 #include <string>
 #include <locale>
 
-int main() {
+ int main()
+ {
     std::setlocale(LC_ALL, "es_MX.UTF-8");
-    std::cout << "Piensa en un Videojuego (Halo infinite, Star Wars BF2)." << std::endl;
-    std::cout << "Responda 1 para 'si' o 0 para 'no' a las preguntas." << std::endl;
+    std::cout << "Piensa en un Videojuego (Halo, Fortnite, COD, Gears, Minecraft, Subnautica, No mansky, Starfield)." << std::endl;
+    std::cout << "Responda 's' o 'n' a las preguntas." << std::endl;
     std::string respuesta;
     bool repetir = true;
 
     while (repetir)
     {
-     
-        bool = true;
-        1 
-        std::cout << "Deseas repetir el algoritmo? 0.- NO 1.- SI: ";
-        std::cin >> ;
-        system("cls");
+        
+        //pregunta 1:
+        std::cout << "Tu juego es de disparos? (s/n):\n"; 
+        std::cin >> respuesta; 
+        if (respuesta == "s")
+        {
+            //pregunta 2: 
+            std::cout << "Es en primera persona? (s/n):\n";
+            std::cin >> respuesta;
 
+            if (respuesta == "s")
+            {
+                //pregunta 3:
+                std::cout << "Hay alienigenas? (s/n):\n";
+                std::cin >> respuesta;
+                if (respuesta == "s")
+                {
+                    //respuesta 1:
+                    std::cout << "El juego es Halo\n";
+                }
+                else
+                {
+                    std::cout << "El juego es COD\n";
+                }
+            }
+
+            else
+            {
+                //pregunta 3:
+                std::cout << "Es tercera persona? (s/n)\n";
+                std::cin >> respuesta;
+
+                if (respuesta == "s")
+                {
+                    //pregunta 4:
+                    std::cout << "Se construye? (s/n)\n";
+                    std::cin >> respuesta;
+                    if (respuesta == "s")
+                    {
+                        //respuesta 2:
+                        std::cout << "Entonces la respuesta es fortnite\n";
+                    }
+                    else
+                    {
+                        std::cout << "Entonces la respuesta es Gears\n";
+                    }
+                }
+            }
+
+        }
+        else
+        { 
+            //pregunta 1:
+            std::cout << "Es un juego de aventura? (s/n)\n";
+            std::cin >> respuesta;
+            if (respuesta == "s")
+            {
+                //pregunta 2:
+                std::cout << "Se usan naves? (s/n)\n";
+                std::cin >> respuesta;
+
+                if (respuesta == "s")
+                {
+                    //pregunta 3: 
+                    std::cout << "Es exclusivo de Xbox? (s/n)\n";
+                    std::cin >> respuesta;
+                    if (respuesta == "s")
+                    {
+                        //respuesta 3:
+                        std::cout << "Es Starfield \n";
+                    }
+                    else
+                    {
+                        std::cout << "Es No mansky \n";
+                    }
+
+                }
+                else
+                {
+                    //pregunta 2:
+                    std::cout << "Es de cubos? (s/n)\n";
+                    std::cin >> respuesta;
+                    if (respuesta == "s")
+                    {
+                        //respuesta 4:
+                        std::cout << "Es Minecraft \n";
+                    }
+                    else
+                    {
+                        std::cout << "Es Subnautica \n";
+                    }
+                }
+            }
+            
+        }
+
+            
+        std::cout << "Deseas repetir el algoritmo? 0.- NO 1.- SI:\n"; 
+        std::cin >> repetir; 
+        system("cls"); 
     }
-    return 0; 
-
-}
+          
+ }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
 // Depurar programa: F5 o menú Depurar > Iniciar depuración
