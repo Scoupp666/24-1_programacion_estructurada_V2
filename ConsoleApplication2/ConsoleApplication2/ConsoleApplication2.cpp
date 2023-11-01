af3b99458646a8dc3730795466f2266ba3dbfdcd
Scoupp666 <144696601+Scoupp666@users.noreply.github.com>
2023-10-31 19:25:26 -0600

    P016

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,68 +1,81 @@
-// ConsoleApplication2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// P015_Batalla_Pokemon_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// Diego Delfin Garibi, zap994, 24/10/2023
+// Realizar un simulador de batalla Pokémon.
 
 #include <iostream>
 #include <windows.h>
 #include <locale.h>
 #include <math.h> 
 #include <string>
+#include <cstdlib> 
 
 int main()
 {
-    std::string pikachu;
-    std::string charizard;
-    std::string respuesta;
-    bool pokemon;
+    int pikachu; //Variable para pikachu vida
+    int charizard; //Variable para charizard vida
+    std::string respuesta; //cadena de caracteres para poder responder
+    bool pokemon; //Variable que engloba a los 2 pokemones para uasr en if
     bool repetir = true;
-    int hp = 5;
-    int ataque = 1;
-    int defensa = 1;
+    int random = 0; //Randomizador, sin uso
+    int hp1 = 2; //Variable de vida para pikachu
+    int hp2 = 2; //Variable de vida para charizard
+    int ataque = 1; // puntos de ataque
+    int defensa = 1; //puntos de defensa
     int opc = 0;
-    std::cout << "Que pokemon escoges (1).- pikachu, (0).- charizard\n";
-    std::cin >> pokemon;
-
-    if (pokemon)
+    while (repetir)
     {
-        std::cout << "Has escogido a pikachu, vas a pelear contra charizard\n";
-    }
-    else
-    {
-        std::cout << "Has escogido a charizard, vas a pelear contra pikachu\n";
-    }
-    std::cout << "Ambos tienen 5 vidas, valorenlas\n";
-
-    std::cout << "atacas o defiendes?\n";
-    std::cin >> respuesta;
-    switch (opc)
-    {
-    case 1:
-        if (ataque == true) 
+        //en esta parte se escoge ek pokemon a usar
+        std::cout << "Que pokemon escoges (1).- pikachu, (0).- charizard\n";
+        std::cin >> pokemon;
+        //una vez escogido el pokemon sigue la pelea, usando bucle while, switch-case y if-else
+        if (pokemon == true)
         {
-            std::cout << "Has atacado a charizard\n";
-            std::cout << "Esperando...\n";
-            std::cout << "Charizard ha perdido una vida\n";
+            std::cout << "Has escogido a pikachu, vas a pelear contra charizard\n";
         }
         else
         {
-            std::cout << "Charizard ha decidido atacarte pero te has defendido\n";
-            std::cout << "Espera...\n";
-            std::cout << "No has perdido vida\n";
+            std::cout << "Has escogido a charizard, vas a pelear contra pikachu\n";
         }
-        break; 
+        std::cout << "Ambos tienen 5 vidas, valorenlas\n";
+        std::cout << "Puedes escoger atacar o defender\n";
+
+        switch (opc)
+        {
+        case 1:
+            std::cout << "Ambos tienen 5 vidas, valorenlas\n";
+
+            std::cout << "atacas o defiendes?\n";
+            std::cin >> respuesta;
+            if (ataque == true)
+            {
+                std::cout << "Has atacado a charizard\n";
+                std::cout << "Esperando...\n";
+                std::cout << "Charizard ha perdido una vida\n";
+                hp2 = charizard - ataque;
+                std::cout << "Has ganado\n";
+            }
+            else
+            {
+                std::cout << "Has escogido defensa\n";
+                std::cout << "Espera...\n";
+                std::cout << "Tu salud se mantiene\n";
+            }
+            break;
+
+        case 2:
+
+            std::cout << "atacas o defiendes?\n";
+            std::cin >> respuesta;
+
+            std::cout << "Has escogido defensa\n";
+            std::cout << "Espera...\n";
+            std::cout << "Charizard decidio atacarte";
+            std::cout << "Espera...\n";
+            std::cout << "Tu salud se mantiene\n";
+            break;
+        }
+        std::cout << "Deseas repetir el algoritmo? 0.- NO 1.- SI: ";
+        std::cin >> repetir;
+        system("cls");
     }
-    case 2:
-    {
-        if (defensa == true)
-    }
-    std::cout << "ganaste\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
