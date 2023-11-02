849e193bd98b354426e78e6e77cfcfa6df34f519
Scoupp666 <144696601+Scoupp666@users.noreply.github.com>
2023-11-02 08:00:46 -0600

    P016_V1

--- P016_Captura_de_libros_V0/P016_Captura_de_libros_V0/P016_Captura_de_libros_V0.cpp
+++ P016_Captura_de_libros_V0/P016_Captura_de_libros_V0/P016_Captura_de_libros_V0.cpp
@@ -2,54 +2,57 @@
 // Diego Delfin Garibi, zap994, 30/10/2023
 // Guardar Videojuegos en un arreglo Finito (3 juegos por lo menos).
 
+
+
+
+
 #include <iostream>
-#include <locale.h>
-#include <math.h> 
 #include <string>
+    
 
-
-int main()
-{
-    std::string Juegos[3];
-    std::string autor[3];
-    std::string Developer[3];
-    int Fechaestreno[3]; 
-    bool repetir = true;
-
-    do 
-    { 
-        std::cout << "Ingresa los datos de los juegos \n";
-        for (int i = 0; i <= 3; i++)
-        {
-        if (i >= 1)
-        {
-            std::cin.ignore();
-        }
-            std::cout << "Como se llama el juego?" << i + 1 << std::endl; 
-            getline(std::cin, Juegos[i]); 
-            std::cout << "Quien es su autor?\n";
-            getline(std::cin, autor[i]); 
-            std::cout << "Quien es el desarrollador?\n";
-            getline(std::cin, Developer[i]); 
-            std::cout << "Cuando fue su fecha de estreno?\n";
-            std::cin >> Fechaestreno[i]; 
-        }
-        
-        std::cout << "\n";
-
-        for (int j = 0; j <= 3; j++)  
-        {
-            std::cout << "Como se llama el juego?" << Juegos[j] << j + 1 << std::endl;  
-            std::cout << "Quien es su autor?" << autor[j] << j + 1 << std::endl; 
-            std::cout << "Quien es el desarrollador?" << Developer[j] << j + 1 << std::endl;  
-            std::cout << "Cuando fue su fecha de estreno?" << Fechaestreno[j] << j + 1 << std::endl; 
-        }
-    } while (repetir);
+    int main() 
     {
-        std::cout << "Deseas repetir el algoritmo? 1.- si, 0.- no \n";
-        std::cin >> repetir; 
-        system("cls"); 
+        std::string Juegos[4]; 
+        std::string autor[4]; 
+        std::string Developer[4]; 
+        std::string Fechaestreno[4]; 
+        int repetir = 1;
 
-        }
-}
+        do {
+            std::cout << "Ingresa los datos de los juegos\n";
+
+            for (int i = 0; i < 4; i++) {
+                if (i >= 1) {
+                    std::cin.ignore();
+                }
+                std::cout << "Como se llama el juego " << i + 1 << "? ";
+                getline(std::cin, Juegos[i]);
+                std::cout << "Quien es su autor? ";
+                getline(std::cin, autor[i]);
+                std::cout << "Quien es el desarrollador? ";
+                getline(std::cin, Developer[i]);
+                std::cout << "Cuando fue su fecha de estreno? ";
+                std::cin >> Fechaestreno[i];
+            }
+
+            std::cout << "\n";
+
+            for (int j = 0; j < 4; j++) {
+                std::cout << "Detalles del juego " << j + 1 << ":\n";
+                std::cout << "Nombre del juego: " << Juegos[j] << ":\n";
+                std::cout << "Autor: " << autor[j] << ":\n";
+                std::cout << "Desarrollador: " << Developer[j] << ":\n";
+                std::cout << "Fecha de estreno: " << Fechaestreno[j] << ":\n"; 
+            }
+
+            std::cout << "Deseas repetir el algoritmo? 1 - si, 0 - no\n";
+            std::cin >> repetir;
+            system("cls");
+
+        } while (repetir == 1);
+
+        return 0;
+    }
+
+
 
