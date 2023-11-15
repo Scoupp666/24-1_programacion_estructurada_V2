c547e5e393456b22454b0d3ef01a0bfd896b85eb
Scoupp666 <144696601+Scoupp666@users.noreply.github.com>
2023-11-15 08:42:19 -0600

    P019_Listas_V0

---
+++ P018_Punteros_V0/P018_Punteros_V0/P018_Punteros_V0.cpp
@@ -0,0 +1,12 @@
+// P018_Punteros_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// 
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
---
+++ P019_Listas_V0/P019_Listas_V0/P019_Listas_V0.cpp
@@ -0,0 +1,38 @@
+// P019_Listas_V0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí. 
+// Diego Delfin Garibi, zap994
+// Listas enlazadas
+#include <iostream>
+
+
+class nodo
+{
+public:
+    int edad;
+    nodo* next;
+};
+
+int main()
+{
+    nodo* cabeza = NULL;
+    nodo* torso = NULL;
+    nodo* pies = NULL;
+
+    cabeza = new nodo();
+    torso = new nodo();
+    pies = new nodo();
+
+    cabeza->edad = 25;
+    cabeza->next = torso;
+
+    torso->edad = 15;
+    torso->next = pies;
+
+    pies->edad = 5;
+    pies->next = NULL;
+
+    while (cabeza != NULL)
+    {
+        std::cout << "La edad del nodo es: " << cabeza->edad << std::endl;
+        cabeza = cabeza->next;
+    }
+}
